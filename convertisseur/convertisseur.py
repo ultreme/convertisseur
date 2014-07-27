0110d0607ecc05285e029cb0825aa300b8d81828
Manfred Touron <m@42.am>
2014-07-27 15:56:58 +0200

    Added Convertissage class

--- convertisseur/convertisseur.py
+++ convertisseur/convertisseur.py
@@ -26,11 +26,18 @@ class Convertisseur:
     def get_infos(self):
         value = self.clean_value
         family = self.input_family
-        type_ = type(value)
 
         infos = []
         if family == 'number':
             infos.append('un nombre')
+
+            if value < 0:
+                infos.append('negatif')
+            elif value > 0:
+                infos.append('positif')
+            else:
+                infos.append('nul')
+
             if isinstance(value, int):
                 infos.append('sans virgule')
             elif isinstance(value, float):
@@ -47,18 +54,33 @@ class Convertisseur:
 
         return infos
 
+    def integer_get_results(self):
+        value = self.clean_value
+
+        return [
+            {'in': 'en base 10', 'out': 'en hexadecimal', 'value': hex(value)}
+        ]
+
+
+    def float_get_results(self):
+        return []
+
     def number_get_results(self):
         value = self.clean_value
-        type_ = type(value)
-        return [
+
+        results = [
             {'in': 'kg', 'out': 'g', 'value': value * 1000},
             {'in': 'g', 'out': 'kg', 'value': value / 1000},
             {'in': 'octets', 'out': 'kilo-octets', 'value': value / 1024 },
         ]
+        if isinstance(value, int):
+            results += self.integer_get_results()
+        elif isinstance(value, float):
+            results += self.float_get_results()
+        return results
 
     def string_get_results(self):
         value = self.clean_value
-        type_ = type(value)
         return [
             {'out': 'caracteres de long', 'value': len(value)},
             {'out': 'en verlant', 'value': value[::-1]},
